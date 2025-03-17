04a1edd21142469eebd7714972fe2ba030040540
anishnairva <anishnairva@gmail.com>
2025-03-17 10:46:12 +0530

    test

--- src/helper.py
+++ src/helper.py
@@ -6,7 +6,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
-#Extract Data From the PDF File s
+#Extract Data From the PDF Files
 def load_pdf_file(data):
     loader= DirectoryLoader(data,
                             glob="*.pdf",
